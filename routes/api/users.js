3716ab457af241a8b226a8158dbbaf54777967c7
Prasetya Aji <prasetya.aji@reservasi.com>
2018-08-06 17:43:12 +0700

    validation api Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -64,6 +65,13 @@ router.post('/register', (req,res) => {
 // @desc Login User / Returning JWT Token
 // @access Public
 router.post('/login', (req,res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -71,7 +79,8 @@ router.post('/login', (req,res) => {
         .then(user => {
             // Check for user
             if(!user) {
-                return res.status(404).json({email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             // Check Password
@@ -99,7 +108,8 @@ router.post('/login', (req,res) => {
                             }
                         );
                     } else {
-                        return res.status(400).json({password: 'Password incorrect'})
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors)
                     }
                 });
         });
