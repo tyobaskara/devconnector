ee850de27f791a49fb3598d1b77a45e4fddf10af
Prasetya Aji <prasetya.aji@reservasi.com>
2018-08-15 11:08:59 +0700

    delete profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,7 +10,7 @@ const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
-// Load User Profile
+// Load User Model
 const User = require('../../models/User');
 
 // @route GET api/profile/test
@@ -247,6 +247,21 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
 
             // Save
             profile.save().then(profile => res.json(profile));
+        })
+        .catch(err => res.status(404).json(err));
+})
+
+// @route DELETE api/profile/
+// @desc Delete user and profile
+// @access Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    Profile.findOneAndRemove({ user: req.user.id })
+        .then(profile => {
+            User.findOneAndRemove({ _id: req.user.id}).then(() => {
+                res.json({ success: true })
+            })
+            .catch(err => res.status(404).json(err));
         })
         .catch(err => res.status(404).json(err));
 })
