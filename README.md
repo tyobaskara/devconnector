d849c4d70863d760447a1ca3bcd0d3e6970fb941
Prasetya Aji <prasetya.aji@reservasi.com>
2018-08-23 13:40:09 +0700

    final

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # devconnector
 devconnector mern rebuild
 
+npm install
+npm run dev
+
 // mongodb with mlab.com
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-  'mongodb://admin123:admin123@ds249325.mlab.com:49325/devconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
